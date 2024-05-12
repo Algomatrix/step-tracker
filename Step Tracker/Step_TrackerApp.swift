54ee67cdd5ae735432d07dbe04e40d564f300cfa
Algomatrix <61648392+Algomatrix@users.noreply.github.com>
2024-05-12 23:12:19 +0900

    Xcode project birth
    Could have chosen a more cooler commit name.

---
+++ Step Tracker/Step_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Step_TrackerApp.swift
+//  Step Tracker
+//
+//  Created by Shubham Shetkar on 2024/05/12.
+//
+
+import SwiftUI
+
+@main
+struct Step_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
