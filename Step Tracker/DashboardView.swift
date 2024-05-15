583f1e8c8cbcdd2d9e810ac948978a117893e3bd
Algomatrix <61648392+Algomatrix@users.noreply.github.com>
2024-05-15 23:04:23 +0900

    data list view initial ui

--- Step Tracker/DashboardView.swift
+++ Step Tracker/DashboardView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  DashboardView.swift
 //  Step Tracker
 //
 //  Created by Shubham Shetkar on 2024/05/12.
@@ -22,7 +22,7 @@ case steps, weight
     }
 }
 
-struct ContentView: View {
+struct DashboardView: View {
 
     @State private var selectedStat: HealthMetricContext = .steps
     var isSteps: Bool { selectedStat == .steps }
@@ -90,7 +90,7 @@ struct ContentView: View {
             .padding()
             .navigationTitle("Dashboard")
             .navigationDestination(for: HealthMetricContext.self) { metric in
-                Text(metric.title)
+                HealthDataListView(metric: metric)
             }
         }
         .tint(isSteps ? .pink : .indigo)
@@ -98,5 +98,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    DashboardView()
 }
